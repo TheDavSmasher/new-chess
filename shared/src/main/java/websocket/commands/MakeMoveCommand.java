9796898182d31dbbdea7f2d2091bb6930b76c49a
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-03-07 17:22:14 -0700

    Added Command classes

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,20 +1,13 @@
-package websocket.userCommands;
+package websocket.commands;
 
 import chess.ChessMove;
 
 public class MakeMoveCommand extends UserGameCommand {
-    private final int gameID;
     private final ChessMove move;
 
     public MakeMoveCommand(String authToken, int gameID, ChessMove move) {
-        super(authToken);
-        this.commandType = CommandType.MAKE_MOVE;
-        this.gameID = gameID;
+        super(CommandType.MAKE_MOVE, authToken, gameID);
         this.move = move;
-    }
-
-    public int getGameID() {
-        return gameID;
     }
 
     public ChessMove getMove() {
