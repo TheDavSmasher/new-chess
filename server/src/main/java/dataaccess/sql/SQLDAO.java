ce5ecc1eb5017f425c8796b2a342843b799123e6
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-03-28 18:28:17 -0600

    Created getStatement to reduce duplicate code

--- server/src/main/java/dataaccess/sql/SQLDAO.java
+++ server/src/main/java/dataaccess/sql/SQLDAO.java
@@ -19,37 +19,31 @@ public abstract class SQLDAO {
     }
 
     protected static <T> T tryQuery(@Language("SQL") String sql, SqlQuery<T> query, Object... params) throws DataAccessException {
-        try (Connection connection = DatabaseManager.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(sql)) {
-                setParams(statement, params);
-                try (ResultSet rs = statement.executeQuery()) {
-                    return query.execute(rs);
-                }
-            }
+        try (PreparedStatement statement = getStatement(sql, params); ResultSet rs = statement.executeQuery()) {
+            return query.execute(rs);
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
     }
 
     protected static void tryUpdate(@Language("SQL") String sql, SqlUpdate update, Object... params) throws DataAccessException {
-        try (Connection connection = DatabaseManager.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-                setParams(statement, params);
-                int result = statement.executeUpdate();
+        try (PreparedStatement statement = getStatement(sql, params)) {
+            int result = statement.executeUpdate();
 
-                try (ResultSet rs = statement.getGeneratedKeys()) {
-                    if (rs.next())
-                        result = rs.getInt(1);
-                }
-
-                update.execute(result);
+            try (ResultSet rs = statement.getGeneratedKeys()) {
+                if (rs.next())
+                    result = rs.getInt(1);
             }
+
+            update.execute(result);
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
         }
     }
 
-    private static void setParams(PreparedStatement statement, Object[] params) throws SQLException {
+    private static PreparedStatement getStatement(String sql, Object... params) throws DataAccessException, SQLException {
+        Connection connection = DatabaseManager.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
         int i = 1;
         for (Object param : params) {
             switch (param) {
@@ -58,6 +52,7 @@ public abstract class SQLDAO {
                 case null, default -> statement.setNull(i++, NULL);
             }
         }
+        return statement;
     }
 
     protected static void confirmUpdate(int updateResult) throws DataAccessException {
