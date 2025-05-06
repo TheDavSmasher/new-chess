a0e4a2e7ae49d6f4f0db0ee2e6359fd9f46f486f
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-05 19:10:41 -0600

    File methods reorder

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -12,13 +12,9 @@ public class ChessPosition {
     private final int row;
     private final int col;
 
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ChessPosition that = (ChessPosition) o;
-        return row == that.row && col == that.col;
+    public ChessPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
 
     /**
@@ -37,21 +33,24 @@ public class ChessPosition {
         return col;
     }
 
+    public boolean outOfBounds() {
+        return (row > 8 || row < 1) || (col > 8 || col < 1);
+    }
+
     public String toString() {
         return row + "," + col;
     }
 
-    public boolean outOfBounds() {
-        return (row > 8 || row < 1) || (col > 8 || col < 1);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && col == that.col;
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(row, col);
-    }
-
-    public ChessPosition(int row, int col) {
-        this.row = row;
-        this.col = col;
     }
 }
