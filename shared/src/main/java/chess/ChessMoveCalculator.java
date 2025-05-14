734ddf0110fa8861abcd36817855fef588156eb1
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-14 14:50:01 -0600

    Changed addPromotionMoves to use for loop

--- shared/src/main/java/chess/ChessMoveCalculator.java
+++ shared/src/main/java/chess/ChessMoveCalculator.java
@@ -42,11 +42,16 @@ public class ChessMoveCalculator {
         return endMoves;
     }
 
+    private static final ChessPiece.PieceType[] promotions = {
+        ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.ROOK
+    };
+
     private static void addPawnPromotionMoves(Collection<ChessMove> moves, ChessPosition start, ChessPosition end, ChessGame.TeamColor color) {
-        if (end.getRow() == getTeamInitialRow(getOtherTeam(color))) {
-            Collections.addAll(moves, new ChessMove(start, end, ChessPiece.PieceType.QUEEN), new ChessMove(start, end, ChessPiece.PieceType.ROOK), new ChessMove(start, end, ChessPiece.PieceType.BISHOP), new ChessMove(start, end, ChessPiece.PieceType.KNIGHT));
-        } else {
-            moves.add(new ChessMove(start, end, null));
+        ChessPiece.PieceType[] pieces = end.getRow() == getTeamInitialRow(getOtherTeam(color))
+            ? promotions
+            : new ChessPiece.PieceType[] {null};
+        for (var pieceType : pieces) {
+            moves.add(new ChessMove(start, end, pieceType));
         }
     }
 
