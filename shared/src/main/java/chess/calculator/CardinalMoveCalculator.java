63ccd1bb486abdbcd9938e1962b488e6e5718a76
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-15 20:07:50 -0600

    Combined methods to simplify logic

--- shared/src/main/java/chess/calculator/CardinalMoveCalculator.java
+++ shared/src/main/java/chess/calculator/CardinalMoveCalculator.java
@@ -12,8 +12,19 @@ public abstract class CardinalMoveCalculator extends PieceMoveCalculator {
         for (boolean flipA : options) {
             for (boolean flipB : options) {
                 for (boolean flipC : options) {
-                    endMoves.addAll(getMovesFromLimits(board, start, getLimit(start, flipA, flipB),
-                            getDirMod(true, flipA, flipB, flipC), getDirMod(false, flipA, flipB, flipC)));
+                    int limit = getLimit(start, flipA, flipB);
+                    int rowMod = getDirMod(true, flipA, flipB, flipC);
+                    int colMod = getDirMod(false, flipA, flipB, flipC);
+
+                    for (int i = 1; i <= limit; i++) {
+                        ChessPosition temp = start.offsetBy(i * rowMod, i * colMod);
+                        if (temp.outOfBounds()) continue;
+                        ChessPiece atTemp = board.getPiece(temp);
+                        if (atTemp == null || (atTemp.color() != board.getPiece(start).color())) {
+                            endMoves.add(new ChessMove(start, temp));
+                        }
+                        if (atTemp != null) { break; }
+                    }
                     if (ignoreThird()) break;
                 }
             }
@@ -24,27 +35,4 @@ public abstract class CardinalMoveCalculator extends PieceMoveCalculator {
     protected abstract int getLimit(ChessPosition start, boolean flipA, boolean flipB);
     protected abstract int getDirMod(boolean isRow, boolean flipA, boolean flipB, boolean flipC);
     protected abstract boolean ignoreThird();
-
-    private static Collection<ChessMove> getMovesFromLimits(ChessBoard board, ChessPosition start, int limit, int rowMod, int colMod) {
-        int[][] offsets = new int[limit][];
-        for (int i = 1; i <= limit; i++) {
-            offsets[i - 1] = new int[] { i * rowMod, i * colMod };
-        }
-        return getMovesFromOffsets(board, start, offsets);
-    }
-
-    private static Collection<ChessMove> getMovesFromOffsets(ChessBoard board, ChessPosition start, int[][] offsets) {
-        Collection<ChessMove> endMoves = new ArrayList<>();
-        ChessGame.TeamColor color = board.getPiece(start).color();
-        for (int[] offset : offsets) {
-            ChessPosition temp = new ChessPosition(start.getRow() + offset[0], start.getColumn() + offset[1]);
-            if (temp.outOfBounds()) continue;
-            ChessPiece atTemp = board.getPiece(temp);
-            if (atTemp == null || (atTemp.color() != color)) {
-                endMoves.add(new ChessMove(start, temp, null));
-            }
-            if (atTemp != null) { break; }
-        }
-        return endMoves;
-    }
 }
