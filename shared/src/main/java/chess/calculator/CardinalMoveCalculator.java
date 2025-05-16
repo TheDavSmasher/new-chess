97af57cf88e41f791c38ed8c3b081cfd7931613c
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-15 20:34:18 -0600

    Extracted inner logic to reduce nesting

--- shared/src/main/java/chess/calculator/CardinalMoveCalculator.java
+++ shared/src/main/java/chess/calculator/CardinalMoveCalculator.java
@@ -12,22 +12,27 @@ public abstract class CardinalMoveCalculator extends PieceMoveCalculator {
         for (boolean flipA : options) {
             for (boolean flipB : options) {
                 for (boolean flipC : options) {
-                    for (int i = 1; i <= getLimit(start, flipA, flipB); i++) {
-                        ChessPosition temp = new ChessPosition(
-                                start.row() + i * getDirMod(true, flipA, flipB, flipC),
-                                start.col() + i * getDirMod(false, flipA, flipB, flipC));
-                        if (temp.outOfBounds()) continue;
-                        ChessPiece atTemp = board.getPiece(temp);
-                        if (atTemp == null || (atTemp.color() != board.getPiece(start).color())) {
-                            endMoves.add(new ChessMove(start, temp));
-                        }
-                        if (atTemp != null) { break; }
-                    }
-                    if (ignoreThird()) break;
+                    collectMovesInDirection(board, start, endMoves, flipA, flipB, flipC);
+                    if (ignoreThird()) { break; }
                 }
             }
         }
         return endMoves;
+    }
+
+    private void collectMovesInDirection(ChessBoard board, ChessPosition start, Collection<ChessMove> endMoves,
+                                         boolean flipA, boolean flipB, boolean flipC) {
+        for (int i = 1; i <= getLimit(start, flipA, flipB); i++) {
+            ChessPosition temp = new ChessPosition(
+                    start.row() + i * getDirMod(true, flipA, flipB, flipC),
+                    start.col() + i * getDirMod(false, flipA, flipB, flipC));
+            if (temp.outOfBounds()) continue;
+            ChessPiece atTemp = board.getPiece(temp);
+            if (atTemp == null || (atTemp.color() != board.getPiece(start).color())) {
+                endMoves.add(new ChessMove(start, temp));
+            }
+            if (atTemp != null) { break; }
+        }
     }
 
     protected abstract int getLimit(ChessPosition start, boolean flipA, boolean flipB);
