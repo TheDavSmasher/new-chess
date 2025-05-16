1e0ddb913b82903d5573500f91b3f1209272033b
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-15 19:34:31 -0600

    Simplified boolean expression

--- shared/src/main/java/chess/calculator/KingMoveCalculator.java
+++ shared/src/main/java/chess/calculator/KingMoveCalculator.java
@@ -3,7 +3,6 @@ package chess.calculator;
 public class KingMoveCalculator extends OffsetMoveCalculator {
     @Override
     protected int getDirMod(boolean isRow, boolean flipA, boolean flipB, boolean flipC) {
-        boolean b = flipB ^ !isRow;
-        return getMod(flipC && b, flipB && b) * getMod(flipA);
+        return getMod(flipC && flipB == isRow, flipB && isRow) * getMod(flipA);
     }
 }
