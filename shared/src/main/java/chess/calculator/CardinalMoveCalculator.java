60c9b2fa0e83306ef70d4c631513ca397433ee0b
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-15 20:11:38 -0600

    Inlined variables

--- shared/src/main/java/chess/calculator/CardinalMoveCalculator.java
+++ shared/src/main/java/chess/calculator/CardinalMoveCalculator.java
@@ -12,12 +12,10 @@ public abstract class CardinalMoveCalculator extends PieceMoveCalculator {
         for (boolean flipA : options) {
             for (boolean flipB : options) {
                 for (boolean flipC : options) {
-                    int limit = getLimit(start, flipA, flipB);
-                    int rowMod = getDirMod(true, flipA, flipB, flipC);
-                    int colMod = getDirMod(false, flipA, flipB, flipC);
-
-                    for (int i = 1; i <= limit; i++) {
-                        ChessPosition temp = start.offsetBy(i * rowMod, i * colMod);
+                    for (int i = 1; i <= getLimit(start, flipA, flipB); i++) {
+                        ChessPosition temp = new ChessPosition(
+                                start.row() + i * getDirMod(true, flipA, flipB, flipC),
+                                start.col() + i * getDirMod(false, flipA, flipB, flipC));
                         if (temp.outOfBounds()) continue;
                         ChessPiece atTemp = board.getPiece(temp);
                         if (atTemp == null || (atTemp.color() != board.getPiece(start).color())) {
