f09cea9091a528e1826b6c21ffab84d7b9b8a34a
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-20 16:53:38 -0600

    Turned PieceMoveCalculator into interface

--- shared/src/main/java/chess/calculator/QueenMoveCalculator.java
+++ shared/src/main/java/chess/calculator/QueenMoveCalculator.java
@@ -3,10 +3,11 @@ package chess.calculator;
 import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPosition;
+import chess.PieceMoveCalculator;
 
 import java.util.Collection;
 
-public class QueenMoveCalculator extends PieceMoveCalculator {
+public class QueenMoveCalculator implements PieceMoveCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition start) {
         Collection<ChessMove> endMoves = new CrossMoveCalculator().calculateMoves(board, start);
