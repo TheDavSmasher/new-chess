0cc43fa398c498c5892e95c1ca7ca12be3037597
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2025-05-20 17:15:17 -0600

    Minor refactor

--- shared/src/main/java/chess/calculator/PawnMoveCalculator.java
+++ shared/src/main/java/chess/calculator/PawnMoveCalculator.java
@@ -8,10 +8,14 @@ import static chess.ChessGame.*;
 
 public class PawnMoveCalculator extends ProgrammaticMoveCalculator {
     @Override
-    protected boolean collectMovesInDirection(ChessBoard board, ChessPosition start, Collection<ChessMove> endMoves, boolean forward, boolean dir, boolean ignored) {
+    protected boolean collectMovesInDirection(
+            ChessBoard board, ChessPosition start, Collection<ChessMove> endMoves,
+            boolean forward, boolean dir, boolean ignored) {
         ChessGame.TeamColor color = board.getPiece(start).color();
         int pieceDirection = getTeamDirection(color);
-        ChessPosition temp = new ChessPosition(start.getRow() + getOffset(forward && dir) * pieceDirection, start.getColumn() + getMod(forward, dir));
+        ChessPosition temp = new ChessPosition(
+                start.getRow() + getOffset(forward && dir) * pieceDirection,
+                start.getColumn() + getMod(forward, dir));
         ChessPiece atTemp = board.getPiece(temp);
         if (forward && atTemp != null) { return true; }
         if (forward || atTemp != null && atTemp.color() != color) {
@@ -30,6 +34,7 @@ public class PawnMoveCalculator extends ProgrammaticMoveCalculator {
     }
 
     private static final ChessPiece.PieceType[] promotions = {
-            ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KNIGHT, ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.ROOK
+            ChessPiece.PieceType.QUEEN, ChessPiece.PieceType.KNIGHT,
+            ChessPiece.PieceType.BISHOP, ChessPiece.PieceType.ROOK
     };
 }
